9b37026a1e07ae93a1c02570a70d37fbcafebbca
Rehan Siraj <rsiraj@r2hstudent.org>
2022-04-18 11:19:04 -0400

    updated database link

--- src/pages/ProductsContainer.js
+++ src/pages/ProductsContainer.js
@@ -13,7 +13,7 @@ const ProductsContainer = () => {
             async () => {
 
                 
-                const response = await fetch(`us-cdbr-east-05.cleardb.net/api/products`);
+                const response = await fetch(`https://plantheaven-server.herokuapp.com/api/products`);
                 const content =  await response.json();
                 setAllProducts(content);
                 setFilteredProducts(content);
